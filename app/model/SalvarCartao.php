57fa1eb646f29f7686feef22b3ad9c268301263d
Francisco Santana Cardoso <fsantanagy@gmail.com>
2021-12-14 21:36:32 -0300

    Ajuste config de conexão com banco de dados.

--- app/model/SalvarCartao.php
+++ app/model/SalvarCartao.php
@@ -1,5 +1,7 @@
 <?php
+
     class SalvarCartao{
+        
          function salvar ($tipo, $titulo, $menssagem, $assinatura, $emailRemetente, $emailDestinatario){
              
             $data = [
@@ -12,16 +14,18 @@
              ];
             
              try{
-                 $pdo = new PDO('mysql:host=localhost;dbname=cardyx', 'roo', '');
+                 include("../config/bdConecte.php");
+                 $pdo = new PDO($DBTYPE.':host='.$DBHOST.';dbname='.$DBNOME, $DBUSUARIO, $DBSENHA);
                  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                  $sql = "INSERT INTO Cartao (tipo, titulo, menssagem, assinatura, email_remetente, email_destinatario) VALUES (:tipo, :titulo, :menssagem, :assinatura, :emailRemetente, :emailDestinatario)";
                  $pdo -> prepare($sql)->execute($data);
              }catch(PDOException $e){
                  echo 'Error: ' . $e->getMessage();
              }
-        }
+        
+            }
     }
-    echo $_SERVER['DOCUMENT_ROOT'];
+
     $sal = new SalvarCartao();
     $sal->salvar('Tipo2', 'Feliz natal', 'Para todos um feliz natal', 'Francisco Santana Cardoso', 'teste@teste.com', 'casa@casa.com');
 
