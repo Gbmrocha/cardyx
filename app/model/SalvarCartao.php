0638a9bd5c2bbd210ce21a43c8dad33bac0161fb
Francisco Santana Cardoso <fsantanagy@gmail.com>
2021-12-15 21:27:31 -0300

    Ajuste de arquivo.

--- app/model/SalvarCartao.php
+++ app/model/SalvarCartao.php
@@ -19,14 +19,16 @@
                  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                  $sql = "INSERT INTO Cartao (tipo, titulo, menssagem, assinatura, email_remetente, email_destinatario) VALUES (:tipo, :titulo, :menssagem, :assinatura, :emailRemetente, :emailDestinatario)";
                  $pdo -> prepare($sql)->execute($data);
+                 return ("Dados Salvos");
              }catch(PDOException $e){
-                 echo 'Error: ' . $e->getMessage();
+                 return ('Ocorreu o seguinte erro: ' . $e->getMessage());
              }
         
             }
     }
 
     $sal = new SalvarCartao();
-    $sal->salvar('Tipo2', 'Feliz natal', 'Para todos um feliz natal', 'Francisco Santana Cardoso', 'teste@teste.com', 'casa@casa.com');
-
+    echo ($sal->salvar('Tipo4', 'Feliz natal', 'Para todos um feliz natal', 'Francisco Santana Cardoso', 'teste@teste.com', 'casa@casa.com'));
+    
 ?>
+
