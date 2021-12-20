3dfcd1f0862372ebf7755ece1a04b475bc1a4d92
EdreyC <edreycarneiro.profissional@gmail.com>
2021-12-19 21:43:19 -0300

    Tela estatistica

---
+++ client/_scripts/grafico.js
@@ -0,0 +1,41 @@
+
+const ctx = document.getElementById('myChart');
+  const data = {
+    labels: [
+      'Modelo 1',
+      'Modelo 2',
+      'Modelo 3',
+      'Modelo 4'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      //Bota os valores aqui no 'data'
+      data: [300, 50, 100,50],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+        '#ee82ee'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const config = {
+    type: 'pie',
+    data: data,
+    options:{
+      plugins:{
+        legend:{
+          labels:{
+            font:{
+              size:14,
+              family:"'Arial', sans-serif",
+            
+            }
+          }
+        }
+      }
+    }
+  };
+      const myChart = new Chart(ctx, config);
---
+++ client/src/components/nav-bar.php
@@ -0,0 +1,12 @@
+
+
+<div class="nav-bar">
+
+<h1 class="logo">Cardyx.</h1>
+
+<div class="box-logo">
+    <a>Estatística</a>
+    <a href="../src/pages/contatos.html">Contatos</a>
+</div>
+
+</div>
